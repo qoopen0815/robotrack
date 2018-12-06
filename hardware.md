e63550e2c5937958ce78a35cb4dbe0c80b6206f2
calm0815 <ku_skyline_wa@yahoo.co.jp>
2018-12-06 15:13:53 +0900

    [add]new library

---
+++ hardware.md
@@ -0,0 +1,12 @@
+TA7291Pのpin配置
+pin1  - GND
+pin2  - motor(+ or -)
+pin3  - not connect
+pin4  - PwmOut motorSpeed(p21);
+pin5  - DigitalOut dir1(p19);
+pin6  - DigitalOut dir2(p20);
+pin7  - Vout
+pin8  - external power supply
+pin9  - not connect
+pin10 - motor(+ or -)
+
--- robotrack.cpp
+++ robotrack.cpp
@@ -3,10 +3,14 @@
 #include "QEI.h"
 #include "MPU6050.h"
 #include "Servo.h"
+#include "PIDcontroller.h"
+#include "Eigen/Dense.h"
+
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Empty.h>
 
+#define M_PI 3.14159265358979323846
 #define N 3     // mpuDeg_param => 0:Pitch, 1:Roll
 
 #ifdef TARGET_LPC1768
@@ -21,34 +25,33 @@
 #define QEI_CODE    QEI::X2_ENCODING
 #define MPU_SDA     p28
 #define MPU_SDL     p27
-//#define SERVO_PIN   p29
+#define SERVO_PIN   p29
+
 #else
 #error "You need to specify a pin for the sensor"
 #endif
 
+//debug matrix
+#define PRINT_MATRIX(MATRIX)  \
+ std::printf(#MATRIX "_matrix\r\n"); \
+ for (int i = 0; i < MATRIX.rows(); ++i) \
+ { \
+   for (int j = 0; j < MATRIX.cols(); ++j) \
+   { \
+     std::printf("%.3f\t", MATRIX(i, j)); \
+   } \
+   std::printf("\r\n");             \
+ } \
+using namespace Eigen;
+
+//rosserial
 ros::NodeHandle nh;
 DigitalOut myled(LED1);
-
 void messageCb(const std_msgs::Empty& toggle_msg)
 {
     myled = !myled;   // blink the led
 }
-
 ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb);
-
-/*
-TA7291Pのpin配置
-pin1  - GND
-pin2  - motor(+ or -)
-pin3  - not connect
-pin4  - PwmOut motorSpeed(p21);
-pin5  - DigitalOut dir1(p19);
-pin6  - DigitalOut dir2(p20);
-pin7  - Vout
-pin8  - external power supply
-pin9  - not connect
-pin10 - motor(+ or -)
-*/
 
 //PC-Serial
 Serial pc(USBTX, USBRX);
