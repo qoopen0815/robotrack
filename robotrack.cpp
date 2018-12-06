9d1dd8650306755e82a55156a45b230b36e8a771
calm0815 <ku_skyline_wa@yahoo.co.jp>
2018-12-06 16:43:01 +0900

    [edit]something

--- robotrack.cpp
+++ robotrack.cpp
@@ -70,51 +70,7 @@ Timer t2;  //pid用
 
 double e[3] = {};  //e[0]=k-1, e[1]=k, e[2]=sigma
 
-//PID
-double input_pid(double e[], double y, double r, double Kp, double Ki, double Kd)
-{
-    double ts = t2.read();
-    t2.reset();
-    
-    e[0] = e[1];
-    e[1] = r - y;
-    if (isnan(e[1]) != true) e[2] += e[1];
-    
-    double u = Kp * e[1] + Ki * ts * e[2] + Kd * (e[1] - e[0]) / ts;
-    
-    pc.printf("error = %4.2f\t", e[1]);
-    
-    return -u;
-}
-
-void speedControll(void)    //速度制御
-{   
-    motor1.speed(1.0);
-    pc.printf("input = %4.2f\t", motor1.state());    
-    pc.printf("RPS = %4.2f\r\n", wheel.getRPS());
-} //input_pid(e, wheel.getRPS(), 2.0, 0.3, 0.3, 0)
-
 int main()
 {
-    // //初期化 initialize
-    // float mpuDeg[N] = {};
-    // wheel.reset();
-    // t1.start();
-    // t2.start();
-    // //double e[3] = {};    //e[0]=k-1, e[1]=k, e[2]=sigma
-    
-    // //実行ループ
-    // while(true)
-    // {
-    //     //update
-    //     mpu.getAccelero(mpuDeg);
-        
-    //     //速度制御
-    //     speedControll();   
-    //     motor2.speed(1.0);
-    
-    //     //motor1.speed(input_pid(e, mpuDeg[0]*9, 40, 15, 0.15, 250));
-    //     //pc.printf("input = %4.2f\t", motor1.state());    
-    //     //pc.printf("deg = %4.2f\r\n", mpuDeg[0]*9);
-    // }
+
 }
